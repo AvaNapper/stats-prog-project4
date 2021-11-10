9cf349c958f3ab104ca8b46f26367251dc95e883
AvaNapper <avanap1@gmail.com>
2021-11-10 12:49:53 +0000

    Initial commit with docstring

--- wg17p4.R
+++ wg17p4.R
@@ -1,7 +1,22 @@
 bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100){
+    # Purpose:
+    # Input:  theta - vector of intial values for optimization parameters
+    #         f     - objective function to minimise
+    #                 first argument is vector of optimization parameters
+    #                 second argument is logical, should the gradients of the 
+    #                   objective function w.r.t. parameters be computed
+    #         tol   - convergence tolerance
+    #         maxit - max number of BFGS iterations to try
+    # Output: f     - scalar value of objective func at the minimum
+    #         theta - vector of values of parameters at minimum
+    #         iter  - number of iterations it took to reach maximum
+    #         g     - gradient vector at the minimum
+    #         H     - approx Hessian matrix at the minimum
+
   
   
   
   
+  return(f, theta, iter, g, H)
   
 }
