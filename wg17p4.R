bc10efc872ab1bbf9a91c691dea04ea9d159ef9f
AvaNapper <avanap1@gmail.com>
2021-11-12 16:30:34 +0000

    gradient vector for f

--- wg17p4.R
+++ wg17p4.R
@@ -7,15 +7,23 @@ bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100){
     #                   objective function w.r.t. parameters be computed
     #         tol   - convergence tolerance
     #         maxit - max number of BFGS iterations to try
+    #
+    #
     # Output: f     - scalar value of objective func at the minimum
     #         theta - vector of values of parameters at minimum
     #         iter  - number of iterations it took to reach maximum
     #         g     - gradient vector at the minimum
     #         H     - approx Hessian matrix at the minimum
 
-  
-  
-  
+
+
+  # Finite differencing to find gradiant vector of f
+  fd <- th0 <- theta
+  eps <- 1e-7
+  for (i in 1:length(th0)) { ## loop over parameters
+    th1 <- th0; th1[i] <- th1[i] + eps ## increase th0[i] by eps
+    fd[i] <- (f(th1) - f(th0))/eps ## approximate -dl/dth[i]
+  }
   
   return(f, theta, iter, g, H)
   
