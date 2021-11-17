5bc2fc7fede03afe6693e00e475b8c364a6674ee
Baldur <balbjo22@gmail.com>
2021-11-17 20:56:12 +0000

    Add vararg parameter
    - Remove loads of print lines
    - Add comments to BFS and the helpers
    - Add the ... (vararg) parameter to all invocations of f.

--- wg17p4.R
+++ wg17p4.R
@@ -6,7 +6,7 @@
 # functions such as optim() or nlm().
 require(debug)
 
-finite_diff <- function(theta, f) {
+finite_diff <- function(theta, f, ...) {
   # Purpose: Find gradient vector of f at point theta using finite differencing.
   
   # Input:  theta - Vector, the location where we want to differentiate f
@@ -25,27 +25,26 @@ finite_diff <- function(theta, f) {
   for (i in 1:length(theta)) { ## loop over parameters/dimensions
     th1 <- theta
     th1[i] <- th1[i] + eps ## move eps in dimension i
-    f_dtheta[i] <- (f(th1) - f(theta))/eps ## approximate -df/dth[i]
+    f_dtheta[i] <- (f(th1, ...) - f(theta, ...))/eps ## approximate -df/dth[i]
   }
   
   f_dtheta
 }
 
 # Should we combine this function and the one above?
-get_gradient <- function(f, theta, f_theta) {
+get_gradient <- function(f, theta, f_theta, ...) {
   # Purpose: Get the gradient of function f at point theta.
   
   # Input:  f       - the function whose gradient we want
   #         theta   - vector corresponding to the location where the gradient is calculated
   #         f_theta - the value of the function at the location of interest
   
-  # Output: f_dtheta  - value of gradient, obtained by finite differencing
-  
-  # Check if the function provided has a gradient attribute
-  # If not, calculate gradient vector using finite differencing
+  # Output: f_dtheta  - value of gradient, obtained by finite differencing or provided as 
+  #                   - "gradient" attribute on f_theta
+
   f_grad <- attr(f_theta, "gradient")
   if (is.null(f_grad)) {
-    f_dtheta <- finite_diff(theta, f)
+    f_dtheta <- finite_diff(theta, f, ...)
   } else {
     f_dtheta <- f_grad
   }
@@ -74,7 +73,7 @@ has_converged <- function(f_val, f_grad, fscale, tol) {
   }
 } 
 
-second_wolfe <- function(delta, f, theta) {
+second_wolfe <- function(delta, f, theta, ...) {
   # Purpose: Check if the gradient does not increase (Wolfe condition 2)
   
   # Input:  delta - vector representing the step length
@@ -88,15 +87,18 @@ second_wolfe <- function(delta, f, theta) {
   # If the old gradient is smaller than the new one, the condition is not satisfied.
   # Otherwise, condition is met and function returns TRUE
   theta_delta <- theta + delta
-  theta_delta_grad <- finite_diff(theta_delta, f)
+  f_theta_delta <- f(theta_delta, ...)
+  theta_delta_grad <- get_gradient(f, theta_delta, f_theta_delta, ...)
   left_side <- t(theta_delta_grad) %*% delta
-  right_side <- 0.9 * t(finite_diff(theta, f)) %*% delta
+  
+  f_theta <- f(theta, ...)
+  right_side <- 0.9 * t(get_gradient(f, theta, f_theta, ...)) %*% delta
   
   left_side >= right_side
 }
 
-reduces_obj <- function(f, theta, step) {
-  # Purpose: Check if the objective function has decreased (Wolfe condition 1)
+reduces_obj <- function(f, theta, step, ...) {
+  # Purpose: Check if the objective function has decreased
   
   # Input:  f     - function which is being optimised
   #         theta - vector representing location before step
@@ -104,22 +106,52 @@ reduces_obj <- function(f, theta, step) {
   
   # Output: boolean indicating if the condition was satisfied
   
-  f(theta) > f(theta + step)
+  f(theta, ...) > f(theta + step, ...)
 }
 
 enqueue <- function(list, ...) {
+  # Purpose:  Enqueue ... in the queue 'list'
+  #           This function along with an appropriate dequeue function turns list
+  #           into a FIFO queue.
+  
+  # Input:    list  - The queue that will now contain all items in '...'
+  #           '...'   - Items to put in list
+  
+  # Output: a queue where the last item out is '...'
+  
   list <- c(list, list(...))
   list <- list[!duplicated(list)]
   list
 }
 
 dequeue <- function(list) {
+  # Purpose:  Return the item in list that has been in list the longest and 
+  #           remove it from list.
+
+  # Input:    list  - The queue to remove from.
+
+  # Output:   Another list. The first item in it is the item from list.
+  #           The second is list with the item removed.
+  
   first <- list[[1]]
   list <- list[-1]
   list(first, list)
 }
 
-bfs <- function(step_v, f, theta) {
+bfs <- function(step_v, f, theta, ...) {
+  # Purpose:  Perform a bfs to find a vector that is a multiple of step_v,
+  #           satisfies the second wolfe and decreases the value of f.
+  
+  # Input:    step_v  - Initial step vector. All values tested are on this line.
+  #           f       - The objective function. Used to evaluate wolfe and check if 
+  #                     our steps are indeed optimizing.
+  #           theta   - Our starting point.
+  #           '...'   - A variable argument parameter that is passed to f each time 
+  #                     it is called.
+    
+  # Output:   The first vector that lies on the line from theta to step_v,
+  #           satisfies the second wolfe and decreases f.
+  
   Q <- list(step_v)
   # Maybe set val to infinity?
   val <- step_v
@@ -132,18 +164,8 @@ bfs <- function(step_v, f, theta) {
     val <- q_v[[1]]
     Q <- q_v[[2]]
     
-    print("Finding step length. Testing:")
-    print(val)
-    print("queue size:")
-    print(length(Q))
-    
-    # Sufficient conditions for good step length    
-    if (second_wolfe(val, f, theta) & reduces_obj(f, theta, val)) {
-      break
-    }
-    
     # Have we gone too far and increased the target value?
-    if (!reduces_obj(f, theta, val)) {
+    if (!reduces_obj(f, theta, val, ...)) {
       # Since this is all based on local approximations, we do not stray too far from the original step
       next_val <- val/2
     } else if (!second_wolfe(val, f, theta)) {
@@ -158,16 +180,13 @@ bfs <- function(step_v, f, theta) {
     count <- count + 1
   }
   
-  print("found step length in ")
-  print(count)
-  print("number of iterations")
   val
 }
 
-get_step <- function(B, f, theta, grad_theta0) {
-  f_theta <- f(theta)
+get_step <- function(B, f, theta, grad_theta0, ...) {
+  f_theta <- f(theta, ...)
   
-  #This is our step vector
+  #Our initial step vector. We might change the length
   step <- drop(-B %*% grad_theta0)
   
   if(all(step == 0)) {
@@ -175,11 +194,9 @@ get_step <- function(B, f, theta, grad_theta0) {
     return(step)
   }
 
-  #Perform a bfs search on [theta, theta +step] to find a step length that:
+  #Perform a breadth first search(bfs) on [theta, theta +step] to find a length that:
   # 1. Decreases the objective(f) 2. Satisfies the second wolfe condition
-  n_step <- bfs(step, f, theta)
-  print("Found new step")
-  print(step)
+  n_step <- bfs(step, f, theta, ...)
   
   n_step
 }
@@ -203,8 +220,6 @@ bfgs <- function(theta, f, ..., tol=1e-5, fscale=1, maxit=100) {
 
   # Set input theta as our initial theta
   theta0 <- theta
-  print('Initial theta')
-  print(theta0)
 
   # Initial B matrix
   B0 <- diag(length(theta0))
@@ -217,53 +232,37 @@ bfgs <- function(theta, f, ..., tol=1e-5, fscale=1, maxit=100) {
     f_theta0 <- f(theta0, ...)
     if (is.na(f_theta0))  stop("function is not defined at initial theta")
   
-    grad_theta0 <- get_gradient(f, theta0, f_theta0)
+    grad_theta0 <- get_gradient(f, theta0, f_theta0, ...)
     for (i in 1:length(grad_theta0)) {
       if (is.na(grad_theta0[i])) stop("derivatives are not defined at initial theta")
     }
 
     # Quasi Newton step from theta0 to theta1 
-    delta <- get_step(B, f, theta0, grad_theta0)#drop(-B %*% grad_theta0)
-    print("delta")
-    print(delta)
-    
+    delta <- get_step(B, f, theta0, grad_theta0, ...)
     
     # New theta value 
     theta1 <- theta0 + delta
-    print('new theta val')
-    print(theta1)
     
     f_theta1 <- f(theta1, ...)
-    grad_theta1 <- get_gradient(f, theta1, f_theta1)
-    print('grad vec of f at new theta')
-    print(grad_theta1)
+    grad_theta1 <- get_gradient(f, theta1, f_theta1, ...)
 
     # Initial s and y vectors. Can this just be delta?
     step_k <- theta1 - theta0
     step_k_t <- t(step_k)
     y_k <- grad_theta1 - grad_theta0
-    
-    print("step_k")
-    print(step_k)
-    
-    print("step_k_t")
-    print(step_k_t)
 
     rho_inv_k <- drop(step_k_t %*% y_k)
     
     # rho_inv_k is a scalar
     rho_k <- 1/rho_inv_k
-    print('rho')
-    print(rho_k)
 
     # Calculate next B matrix
     expression1 <- (diag(length(theta)) - rho_k*(step_k %*% t(y_k)))
     expression2 <- (diag(length(theta)) - rho_k*(y_k %*% step_k_t))
     expression3 <- rho_k * step_k %*% step_k_t
+    
     # Is there some optimization to be done here by first calculating the rightmost matrix multiplication?
     B <- expression1 %*% B %*% expression2 + expression3
-    print('new B')
-    print(B)
     
     if (reduces_obj(f, theta0, delta) == FALSE & has_converged(f_theta0, grad_theta0, fscale, tol) == FALSE)
       warning("objective not reduced and convergence not met")
@@ -281,8 +280,8 @@ bfgs <- function(theta, f, ..., tol=1e-5, fscale=1, maxit=100) {
   for (i in 1:length(theta0)) {
     theta_temp <- theta0
     theta_temp[i] <- theta_temp[i] + eps
-    f_theta_temp <- f(theta_temp,...)
-    grad_temp <- get_gradient(f, theta_temp, f_theta_temp)
+    f_theta_temp <- f(theta_temp, ...)
+    grad_temp <- get_gradient(f, theta_temp, f_theta_temp, ...)
     Hfd[i,] <- (grad_temp - grad_theta0) / eps
   }
   
@@ -290,7 +289,7 @@ bfgs <- function(theta, f, ..., tol=1e-5, fscale=1, maxit=100) {
     warning("max iterations reached without convergence")
   
   bfgs_res = list(
-    f = f(theta0),
+    f = f(theta0, ...),
     theta = theta0,
     iter = iter,
     g = grad_theta0,
@@ -319,8 +318,13 @@ quad <- function(theta, getg=FALSE, m=10) {
   (x^2 + 10*x -5) + y^2 + m
 }
 
-bfgs(c(2, 3), quad, getg=FALSE, maxit = 40)
+bfgs(c(2, 3), m=33, quad, getg=FALSE, maxit = 40)
 
+
+bfgs(c(2, 3), rb, getg=FALSE, maxit = 40)
+
+
+optim(c(-1, 2), rb_1, method = "BFGS", hessian = TRUE)
 
 optim(2, quad, method = "BFGS", hessian = TRUE)
 nlm(rb, c(-1, 2), hessian = TRUE)
